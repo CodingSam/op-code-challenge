ee730cfb304e8add1b17ce7c7e30a778c996c4bf
Samuel Backman <samuel@qvik.fi>
2018-04-11 22:20:23 +0300

    - fixed a couple of bugs - added animations

--- src/components/accordionItem.js
+++ src/components/accordionItem.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import chevronUp from '../icons/chevron-u.svg'
+import React, { Component } from 'react'
 import chevronDown from '../icons/chevron-d.svg'
 import AccordionItemTitle from './accordionItemTitle';
 import AccordionItemDescription from './accordionItemDescription';
 
-const AccordionItem = ({children, active, onClick, title, description}) => {
+class AccordionItem extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      height: 0
+    }
+  }
 
-  const chevron = active ? chevronUp : chevronDown
-  const content = children
+  componentDidMount() {
+    window.requestAnimationFrame(() => {
+      this.setState({height: this.refs["accordionItemContentContainer"].children[0].clientHeight + 1})
+    })
+  }
 
-  return (
-    <div onClick={onClick} className={`accordionItem${active ? ' active': ''}`}>
-      <div className="accordionItemInfoContainer">
-        <div className="accordionItemInfo">
-          <AccordionItemTitle>{title}</AccordionItemTitle>
-          <AccordionItemDescription>{description}</AccordionItemDescription>
+  render() {
+    const {children, active, onClick, title, description} = this.props
+    const chevron = chevronDown
+    const content = children
+    const height = active ? this.state.height : 0
+
+    return (
+      <div onClick={onClick} className={`accordionItem${active ? ' active': ''}`}>
+        <div className="accordionItemInfoContainer">
+          <div className="accordionItemInfo">
+            <AccordionItemTitle>{title}</AccordionItemTitle>
+            <AccordionItemDescription>{description}</AccordionItemDescription>
+          </div>
+          <img src={chevron} className="chevron" alt="chevron" />
         </div>
-        <img src={chevron} className="chevron" alt="chevron" />
+        <div style={{height}} ref="accordionItemContentContainer" className="accordionItemContentContainer">
+          {content}
+        </div>
       </div>
-      {active ? content : null}
-    </div>
-  )
+    )
+  }
 }
 
 export default AccordionItem
